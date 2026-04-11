fa4dfa86289c5bc005265ab8ed12c4493bda2146
BrandaoRM01 <raulmb231@gmail.com>
2026-04-11 18:37:23 -0300

    Adicionando botão de olho para ver a senha nas páginas e ajustando HTMLs

--- projeto/static/js/main.js
+++ projeto/static/js/main.js
@@ -1,13 +1,41 @@
 let input_foto = document.getElementById('foto-imagem');
 let preview_imagem = document.getElementById('preview-imagem');
 
-preview_imagem.addEventListener('click', () => {
-    input_foto.click();
-});
+if (input_foto && preview_imagem) {
 
-input_foto.addEventListener('change', (event) => {
-    let file = event.target.files[0];
-    if (file) {
-        preview_imagem.src = URL.createObjectURL(file);
-    }
+    preview_imagem.addEventListener('click', () => {
+        input_foto.click();
+    });
+
+    input_foto.addEventListener('change', (event) => {
+        let file = event.target.files[0];
+        if (file) {
+            preview_imagem.src = URL.createObjectURL(file);
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    let eyes = document.querySelectorAll(".toggle-password");
+
+    eyes.forEach(el => {
+        el.addEventListener("click", function () {
+
+            let targetId = this.getAttribute("data-target");
+            let input = document.getElementById(targetId);
+            let icon = document.getElementById("eye-" + targetId);
+
+            if (!input || !icon) return;
+
+            if (input.type === "password") {
+                input.type = "text";
+                icon.src = "/static/img/default/close_eye.svg";
+            } else {
+                input.type = "password";
+                icon.src = "/static/img/default/open_eye.svg";
+            }
+        });
+    });
+
 });
