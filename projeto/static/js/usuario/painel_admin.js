78d2e1ec673ab0b2a187eb5518be8e4fbd5fb9f0
BrandaoRM01 <raulmb231@gmail.com>
2026-09-13 19:23:45 -0300

    Atribuindo links do painel admin via js

--- projeto/static/js/usuario/painel_admin.js
+++ projeto/static/js/usuario/painel_admin.js
@@ -3,4 +3,26 @@ import { protegerRota } from '../main.js';
 document.addEventListener('DOMContentLoaded', async () => {
     let usuario = await protegerRota(['admin', 'superadmin']);
     if (!usuario) return;
-})
+
+    configurarLinksDoPainel();
+});
+
+function configurarLinksDoPainel() {
+    const links = {
+        'link-gerenciar-categorias': '/admin/gerenciar-categorias',
+        'link-gerenciar-pontos': '/admin/gerenciar-pontos',
+        'link-gerenciar-promocoes': '/admin/gerenciar-promocoes',
+        'link-gerenciar-usuarios': '/admin/gerenciar-usuarios',
+        'link-gerenciar-ecossistemas': '/admin/gerenciar-ecossistemas',
+        'link-gerenciar-tipos-culturais': '/admin/gerenciar-tipos-culturais',
+        'link-gerenciar-destaques': '/admin/gerenciar-destaques',
+        'link-gerenciar-sugestoes': '/admin/gerenciar-sugestoes'
+    };
+
+    for (let id in links) {
+        document.getElementById(id).addEventListener('click', (evento) => {
+            evento.preventDefault();
+            window.location.href = links[id];
+        });
+    }
+}
